513c3b61d23e7315cc5235b830472adee2a8b31f
Jason Nall <jsonnull@gmail.com>
2023-10-22 15:27:36 -0400

    Update home-manager to use correct architectures.

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,39 +2,32 @@
   description = "Home Manager configuration of jsonnull";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."macbook" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    homeConfigurations.macbook = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ~/configuration/home-manager/jsonnull-macbook.nix ];
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [ ~/configuration/home-manager/jsonnull-macbook.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-
-      homeConfigurations."wsl" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ~/configuration/home-manager/jsonnull-wsl.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
     };
+
+    homeConfigurations.wsl = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [ ~/configuration/home-manager/jsonnull-wsl.nix ];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+    };
+  };
 }
