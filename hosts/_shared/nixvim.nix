b80f4f10b7b3e27a21e073bbfe9609f435f27af7
Jason Nall <jsonnull@gmail.com>
2024-10-18 01:13:15 -0400

    Finish migrating config.

--- hosts/_shared/nixvim.nix
+++ hosts/_shared/nixvim.nix
@@ -8,7 +8,27 @@
     #  settings.background = "hard";
     #};
 
+    # colorscheme-adjacent properties
+    opts.background = "dark";
+    opts.termguicolors = true;
+
     enable = true;
+
+    autoCmd = [
+      {
+        command = "set filetype=jsonc";
+        event = [ "BufNewFile" "BufRead" ];
+        pattern = [ "tsconfig.json" "devcontainer.json" ];
+      }
+    ];
+
+    globals = {
+      # Disabling netrw, recommended by nvim-tree  
+      loaded_netrw = 1;
+      loaded_netrwPlugin = 1;
+
+      mapleader = ",";
+    };
 
     keymaps = [
       {
@@ -16,9 +36,86 @@
         key = "<c-n>";
         mode = "n";
       }
+      {
+        action = "<cmd>Telescope buffers<cr>";
+        key = "<leader>b";
+        mode = "n";
+      }
+      {
+        action = "<cmd>Bdelete<cr>";
+        key = "<leader>w";
+        mode = "n";
+      }
+      {
+        action = "<cmd>Bwipeout<cr>";
+        key = "<leader>q";
+        mode = "n";
+      }
+      {
+        action = "<cmd>:SymbolsOutline<cr>";
+        key = "<c-t>";
+        mode = "n";
+      }
+      {
+        action = "yy";
+        key = "Y";
+        mode = "n";
+      }
+      {
+        action = ":noh<cr>";
+        key = "<leader>h";
+        mode = "n";
+      }
     ];
 
     nixpkgs.pkgs = pkgs.unstable;
+
+    opts = {
+      # systematic
+      encoding = "utf-8";
+      fileencoding = "utf-8";
+      fileencodings = [ "utf-8" ];
+      backup = false; # no .bak
+      swapfile = false; # no .swap
+      undofile = true; # use undo file
+      updatetime = 300; # time (in ms) to write to swap file
+      timeoutlen = 300; # time (in ms) to write to swap file
+      # buffer
+      expandtab = true  ;# expand tab
+      tabstop = 4       ;# tab stop
+      softtabstop = 4   ;# soft tab stop
+      autoindent = true ;# auto indent for new line
+      shiftwidth = 4    ;# auto indent shift width
+      # window
+      number = true;
+      #relativenumber = true;
+      # editing
+      whichwrap = "b,s,<,>,[,]"                ;# cursor is able to move from end of line to next line
+      backspace = [ "indent" "eol" "start" ] ;# backspace behaviors
+      list = false                             ;# show tabs with listchars
+      ignorecase = false                       ;# search with no ignore case
+      hlsearch = true                          ;# highlight search
+      incsearch = false                        ;# no incremental search
+      inccommand = "nosplit"                   ;# live substitute preview
+      completeopt = [ "menu" "menuone" "noselect" ];
+      hidden = true;
+      cursorline = true         ;# show cursor line
+      ruler = false             ;# show ruler line
+      colorcolumn = "120"    ;# display a color column when line is longer than 120 chars
+      signcolumn = "yes"        ;# show sign column (column of the line number)
+      mouse = "nv"              ;# enable mouse under normal and visual mode
+      showmatch = true          ;# show bracket match
+      cmdheight = 1             ;# height of :command line
+      wildmenu = true           ;# wildmenu, auto complete for commands
+      wildmode = [ "longest" "full" ];
+      splitright = true         ;# split to right
+      splitbelow = true         ;# split to below
+      clipboard = "unnamedplus" ;# share system clipboard
+      # tree-sitter code folding
+      foldmethod = "expr";
+      foldexpr = "nvim_treesitter#foldexpr()";
+      foldlevelstart = 99;
+    };
 
     plugins.auto-session = {
       enable = true;
