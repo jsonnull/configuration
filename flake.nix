64c660aca7d13006aa5f544c1f3eb536f6f4118b
Jason Nall <jsonnull@gmail.com>
2024-11-28 10:20:48 -0500

    System upgrade.

--- flake.nix
+++ flake.nix
@@ -14,24 +14,14 @@
     alacritty-theme.url = "github:alexghr/alacritty-theme.nix";
   };
 
-  outputs =
-    inputs@{ self
-    , nixpkgs
-    , home-manager
-    , nixvim
-    , ...
-    }:
-    let
-      inherit (self) outputs;
-    in
-    {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, ... }:
+    let inherit (self) outputs;
+    in {
       overlays = import ./overlays.nix { inherit inputs; };
 
       # hosts
       nixosConfigurations.renderer = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs outputs;
-        };
+        specialArgs = { inherit inputs outputs; };
         system = "x86_64-linux";
         modules = [
           ./hosts/renderer/configuration.nix
@@ -40,9 +30,7 @@
             home-manager.backupFileExtension = "backup";
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.sharedModules = [
-              nixvim.homeManagerModules.nixvim
-            ];
+            home-manager.sharedModules = [ nixvim.homeManagerModules.nixvim ];
             home-manager.users.json = import ./hosts/renderer/home.nix;
           }
         ];
--- hosts/renderer/configuration.nix
+++ hosts/renderer/configuration.nix
@@ -1,19 +1,13 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ config
-, inputs
-, outputs
-, pkgs
-, ...
-}:
+{ config, inputs, outputs, pkgs, ... }:
 
 {
-  imports =
-    [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   nixpkgs = {
     overlays = [
@@ -45,7 +39,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-9f4a94c8-8b60-405b-bbe9-42c13c394de0".device = "/dev/disk/by-uuid/9f4a94c8-8b60-405b-bbe9-42c13c394de0";
+  boot.initrd.luks.devices."luks-9f4a94c8-8b60-405b-bbe9-42c13c394de0".device =
+    "/dev/disk/by-uuid/9f4a94c8-8b60-405b-bbe9-42c13c394de0";
 
   networking.hostName = "renderer"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -146,36 +141,40 @@
 
   services.hardware.openrgb.enable = true;
 
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "Iosevka" "IosevkaTerm" ]; })
-  ];
+  fonts.packages = with pkgs;
+    [ (nerdfonts.override { fonts = [ "Iosevka" "IosevkaTerm" ]; }) ];
 
   # Make the system work with nvidia card
   environment.sessionVariables.CUDA_PATH = "${pkgs.cudatoolkit}";
-  environment.sessionVariables.LD_LIBRARY_PATH = "${pkgs.linuxPackages.nvidia_x11}/lib";
+  environment.sessionVariables.LD_LIBRARY_PATH =
+    "${pkgs.linuxPackages.nvidia_x11}/lib";
   #environment.sessionVariables.LD_LIBRARY_PATH = "${pkgs.cudatoolkit}/lib";
-  environment.sessionVariables.VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
+  environment.sessionVariables.VK_DRIVER_FILES =
+    "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
   environment.sessionVariables.PROTON_ENABLE_NVAPI = "1";
   environment.sessionVariables.DXVK_ENABLE_NVAPI = "1";
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
     # For 32 bit applications
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      rocm-opencl-icd
-      rocm-opencl-runtime
-    ];
+    enable32Bit = true;
+    # extraPackages = with pkgs; [ rocm-opencl-icd rocm-opencl-runtime ];
   };
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     # Modesetting is required
     modesetting.enable = true;
+
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
     powerManagement.enable = false;
+
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
+
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
     # Support is limited to the Turing and later architectures. Full list of
@@ -184,15 +183,22 @@
     # Only available from driver 515.43.04+
     # Do not disable this unless your GPU is unsupported or if you have a good reason to.
     open = true;
+
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
   boot.initrd.kernelModules = [ "nvidia" ];
+  boot.kernelPackages = pkgs.linuxPackages_6_10;
+  # fix for nVidia wayland plasma 6
+  boot.kernelParams = [
+    "nvidia-drm.fbdev=1"
+    "nvidia-drm.modeset=1"
+  ];
   boot.blacklistedKernelModules = [ "nouveau" ];
-
-
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
