c674a2713b0323cc83bfb687b8f070d2b8171220
Jason Nall <jsonnull@gmail.com>
2025-10-18 17:03:48 -0400

    ALVR -> WiVRN

--- modules/nixos/vr/default.nix
+++ modules/nixos/vr/default.nix
@@ -1,11 +1,30 @@
 {
+  pkgs,
   ...
 }:
 {
   programs.adb.enable = true;
-  programs.alvr = {
+
+  services.monado = {
     enable = true;
-    #package = pkgs.alvr-passthrough;
+    defaultRuntime = true;
+  };
+
+  services.wivrn = {
+    enable = true;
     openFirewall = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    opencomposite
+  ];
+
+  snowfallorg.users.json.home.config = {
+    xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+      {
+        "version": 1,
+        "runtime": [ "${pkgs.opencomposite}/bin/opencomposite" ]
+      }
+    '';
   };
 }
