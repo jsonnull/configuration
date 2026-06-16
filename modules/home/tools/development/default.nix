c438f0a8b94d1b8e3a4f2caacfce2bdf9bc984b1
Jason Nall <jsonnull@gmail.com>
2026-06-16 11:46:40 -0400

    Add mkcert local certificate setup

--- modules/home/tools/development/default.nix
+++ modules/home/tools/development/default.nix
@@ -6,6 +6,7 @@
 }:
 let
   cfg = config.tools.dev-general;
+  caRoot = "${config.home.homeDirectory}/.local/share/mkcert";
 in
 {
   options.tools.dev-general.enable = lib.mkEnableOption "Enable general development tools";
@@ -13,6 +14,24 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       gh
+      mkcert
+      nssTools
     ];
+
+    home.sessionVariables = {
+      CAROOT = caRoot;
+      NODE_EXTRA_CA_CERTS = "${caRoot}/rootCA.pem";
+    };
+
+    home.activation.installMkcertRoot = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      export CAROOT="${caRoot}"
+      export PATH="${pkgs.mkcert}/bin:${pkgs.nssTools}/bin:$PATH"
+
+      mkdir -p "$CAROOT"
+
+      if ! TRUST_STORES=nss mkcert -install; then
+        echo "warning: mkcert NSS trust setup failed; run 'TRUST_STORES=nss mkcert -install' manually"
+      fi
+    '';
   };
 }
